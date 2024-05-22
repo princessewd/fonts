669d218c43e3a592ce99dc7c322a53e8b7440d42
princessewd <146062913+princessewd@users.noreply.github.com>
2024-05-22 20:29:10 +0200

    Update raquette.js

--- raquette.js
+++ raquette.js
@@ -2,14 +2,11 @@
 
 var raquette = function() {
     function verifierCaracteres(texte) {
-        // Expression régulière pour vérifier si le texte contient uniquement des lettres et des chiffres
         var regex = /^[a-zA-Z0-9]+$/;
 
-        // Vérifier chaque caractère du texte
         for (var i = 0; i < texte.length; i++) {
             var caractere = texte.charAt(i);
 
-            // Vérifier si le caractère ne correspond pas à l'expression régulière
             if (!regex.test(caractere)) {
                 return false;
             }
